1c83ef6fe4377039dc7080e3467040cb3a7ebe64
Julian Moorhouse <julesthecoder@gmail.com>
2020-03-28 21:54:44 +0000

    Generated object subclasses

---
+++ BeginningCoreData/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  BeginningCoreData
+//
+//  Created by Julian Moorhouse on 28/03/2020.
+//  Copyright © 2020 Mindwarp Consultancy Ltd. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
